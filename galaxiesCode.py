c65b006edc14f7411c0a5b8067456128feecb9d7
Gina Markov <ginamarkov@BridgeUp-2.local>
2016-08-10 10:39:24 -0400

    balh

--- galaxiesCode.py
+++ galaxiesCode.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 data_open = open("galaxies.csv", "rU")
 data_in = list(csv.reader(data_open))
 
-
 '''
+
 greenlist =[]
 redlist = []
 
@@ -21,17 +21,18 @@ plt.ylabel("Red Value")
 plt.hexbin(greenlist,redlist)
 '''
 
-
 r_g_list = []
 for item in data_in[1:]:
 	r_g = float(item[2])
 	r_g_list.append(r_g)
-
-
+allcolors = list(range(-1,1))
+plt.xlabel("Color value")
+plt.ylabel("Frequency")
+plt.title("Frequency of different colors among galaxies")
+plt.xticks(allcolors,allcolors)
 plt.hist(r_g_list)
 
 
 plt.show()
 plt.close()
-
 data_open.close()

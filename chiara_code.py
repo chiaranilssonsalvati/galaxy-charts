3026548e7753b1de0e216d994e598462b76ee7d1
Chiara Nilsson-Salvati <chiaranilsson-salvati@BridgeUp-001.local>
2016-08-10 11:00:27 -0400

    added stuff

---
+++ chiara_code.py
@@ -0,0 +1,38 @@
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+data_open = open("galaxies.csv", "rU")
+data_in = list(csv.reader(data_open))
+
+'''
+
+greenlist =[]
+redlist = []
+
+for item in data_in[1:]:
+	g_mag = item[0]
+	r_mag = item[1]
+	greenlist.append(g_mag)
+	redlist.append(r_mag)
+
+plt.xlabel("Green Value")
+plt.ylabel("Red Value")
+
+
+plt.hexbin(greenlist,redlist)
+'''
+
+r_g_list = []
+for item in data_in[1:]:
+	r_g = float(item[2])
+	r_g_list.append(r_g)
+allcolors = list(range(-1,1))
+plt.xlabel("Color value")
+plt.ylabel("Frequency")
+plt.title("Frequency of different colors among galaxies")
+plt.hist(r_g_list, range=[0, 1.3], align='mid', bins=np.arange(0, 1.4, .05))
+
+
+plt.show()
+plt.close()
+data_open.close()
